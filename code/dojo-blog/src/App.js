c8297b5e0e71da49257c45a9b7542be6ac97a4bf
Larry Jones <mrwizard82d1@gmail.com>
2024-07-04 17:56:15 -0500

    Render an integer variable to the page

--- code/dojo-blog/src/App.js
+++ code/dojo-blog/src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 
 function App() {
     const title = "Welcome to the New Blog";
+    const likes = 50;
+
 
     return (
         <div className="App">
             <div className="content">
                 <h1>{ title }</h1>
+                { /* Note the "unexpected" syntax necessary to embed a comment in the JSX code. */}
+                { /* In addition, note that comments are **not** rendered to the page. */}
+                { /* Even though `likes` is an integer type, it is "automagically" output as a string. */ }
+                <p>Liked { likes } times</p>
             </div>
         </div>
     );
