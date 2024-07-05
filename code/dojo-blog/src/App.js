b07117e405ed8c9209c61337800ae7e023d07bab
Larry Jones <mrwizard82d1@gmail.com>
2024-07-04 19:19:10 -0500

    Allowed output Javascript types
    - Integer (floats)
    - String
    - Array
    - Javascript expressions evaluating to these types

--- code/dojo-blog/src/App.js
+++ code/dojo-blog/src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 function App() {
     const title = "Welcome to the New Blog";
     const likes = 50;
-    const person = { name: 'yoshi', age: 30 };
+    // const person = { name: 'yoshi', age: 30 };
 
     return (
         <div className="App">
             <div className="content">
-                <h1>{ title }</h1>
+                <h1>{title}</h1>
                 { /* Note the "unexpected" syntax necessary to embed a comment in the JSX code. */}
                 { /* In addition, note that comments are **not** rendered to the page. */}
-                { /* Even though `likes` is an integer type, it is "automagically" output as a string. */ }
-                <p>Liked { likes } times</p>
-                <p>{ person }</p>
+                { /* Even though `likes` is an integer type, it is "automagically" output as a string. */}
+                <p>Liked {likes} times</p>
+                { /*<p>{ person }</p>*/}
+
+                <p>{ 10 }</p>
+                <p>{ "Hello, Ninjas!" }</p>
+                <p>{ [1, 2, 3, 4, 5] }</p>
+                <p>{ Math.random() * 10 }</p>
             </div>
         </div>
     );
