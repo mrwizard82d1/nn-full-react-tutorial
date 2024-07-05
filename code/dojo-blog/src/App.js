d6b8f87521a16b865a09ada09a06a2e81af22faf
Larry Jones <mrwizard82d1@gmail.com>
2024-07-04 19:25:41 -0500

    Using Javascript expressions for HTML attributes
    Specifically, the the value of the `href` attribute of a link (anchor) tag.

--- code/dojo-blog/src/App.js
+++ code/dojo-blog/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
     const title = "Welcome to the New Blog";
     const likes = 50;
-    // const person = { name: 'yoshi', age: 30 };
+    // const person = { name: 'yoshi', age: 30 };O
+    const link = 'https://www.google.com';
 
     return (
         <div className="App">
@@ -19,6 +20,8 @@ function App() {
                 <p>{ "Hello, Ninjas!" }</p>
                 <p>{ [1, 2, 3, 4, 5] }</p>
                 <p>{ Math.random() * 10 }</p>
+
+                <a href={ link }>Google Site</a>
             </div>
         </div>
     );
